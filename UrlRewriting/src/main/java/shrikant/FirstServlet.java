47aebe0a00f69984e5d19fb6293d9fd204c5a4c1
shrikant ambatkar <158559554+SHRIKANTAMBATKAR@users.noreply.github.com>
2025-11-14 13:29:31 +0530

    URLRewriting in servlet

---
+++ UrlRewriting/src/main/java/shrikant/FirstServlet.java
@@ -0,0 +1,31 @@
+package shrikant;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("welcome to mr."+request.getParameter("user"));
+		out.print("</br>");
+		out.print("<a href='SecondServlet?uname="+request.getParameter("user")+"'>visite</a>");
+	}
+
+}
